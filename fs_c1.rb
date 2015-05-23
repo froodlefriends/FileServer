020f896599435b3d432b1f1c1d4db28e0fb3c86c
cuddyc <cuddyc@tcd.ie>
2015-05-23 20:18:08 +0100

    Basic Instructions working

---
+++ fs_c1.rb
@@ -0,0 +1,66 @@
+require 'socket'      # Sockets are in standard library
+
+hostname = 'localhost'
+port = 2000
+
+  msg = 'OPEN:'
+  msg += "test.txt\n"
+
+  s = TCPSocket.open(hostname, port)
+
+  s.puts msg
+  message = s.gets
+  puts message
+
+  s.close               # Close the socket when done
+
+  msg = 'READ:'
+  msg += "test.txt\n"
+
+  s = TCPSocket.open(hostname, port)
+
+  s.puts msg
+  message = s.gets
+  f_msg = s.gets
+  puts message
+  p f_msg
+
+  s.close
+
+  msg = 'WRITE:'
+  msg += "test.txt\n"
+  msg += 'MESSAGE: This file was last updated on '
+
+  t_stamp = Time.new
+
+  msg += t_stamp.inspect
+
+  s = TCPSocket.open(hostname, port)
+
+  s.puts msg
+  message = s.gets
+  puts message
+
+  s.close               # Close the socket when done
+
+  msg = 'READ:'
+  msg += "test.txt\n"
+
+  s = TCPSocket.open(hostname, port)
+
+  s.puts msg
+  message = s.gets
+  puts message
+
+  s.close
+
+  msg = 'CLOSE:'
+  msg += "test.txt\n"
+
+  s = TCPSocket.open(hostname, port)
+
+  s.puts msg
+  message = s.gets
+  puts message
+
+  s.close
